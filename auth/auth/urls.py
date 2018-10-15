73758b88f90b06d30b9409523318f583cf24d1cd
Joao Valverde <acci.valverde@gmail.com>
2018-10-15 18:08:29 +0100

    implemented status view

--- auth/auth/urls.py
+++ auth/auth/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path, include
 
+from auth.views import status_view
 
 urlpatterns = [
+    path('', status_view),
     path('jwt/', include('jwt_utils.urls')),
     path('', include('users.urls'))
 ]
