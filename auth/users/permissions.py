a8a25ef300640bcd790ef2c1dbde0ba3c529d69b
Joao Valverde <acci.valverde@gmail.com>
2018-11-17 23:22:40 +0000

    implemented new tsk and endpoint

--- auth/users/permissions.py
+++ auth/users/permissions.py
@@ -5,7 +5,7 @@ class UserPermissions(permissions.BasePermission):
     def has_permission(self, request, view):
         if request.method == 'POST':
             return request.user is None
-        return True
+        return request.user is not None
 
     def has_object_permission(self, request, view, obj):
         return request.user is not None and obj.id == request.user.id
--- auth/users/tasks.py
+++ auth/users/tasks.py
@@ -1,11 +1,29 @@
 from typing import Dict, Any
+
 from django.core.mail import send_mail
 from django.conf import settings
-
 from celery import shared_task
+from rest_framework_jwt.settings import api_settings
+from requests import post
 
 from .models import User
 from .serializers import ConfirmEmailSerializer
+
+
+@shared_task
+def broadcast_registration(user_uuid: str) -> bool:
+    subscribers = [
+        'http://core/profiles',
+    ]
+
+    responses = []
+
+    signed_user_uuid = api_settings.JWT_ENCODE_HANDLER({'uuid': user_uuid})
+
+    for subscriber in subscribers:
+        responses.append(post(url=subscriber, json={'token': signed_user_uuid}))
+
+    return all([response.status_code == 201 for response in responses])
 
 
 @shared_task
@@ -25,4 +43,5 @@ def send_confirmation_email(user: Dict[str, Any]) -> bool:
 
 
 def on_create(user: User) -> None:
+    broadcast_registration.delay(user_uuid=user.uuid)
     send_confirmation_email.delay(user=ConfirmEmailSerializer(user).data)
