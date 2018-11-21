e64f0589b08e3daaedae03bd44c1252b28ad5f62
Joao Valverde <acci.valverde@gmail.com>
2018-11-21 00:18:57 +0000

    updating settings

--- auth/auth/settings/production.py
+++ auth/auth/settings/production.py
@@ -8,4 +8,10 @@ INSTALLED_APPS.append('gunicorn')  # noqa: F405
 ALLOWED_HOSTS = ["*"]
 
 
+JWT_AUTH: dict = {
+    **JWT_AUTH,
+    'JWT_VERIFY_EXPIRATION': True,
+}
+
+
 FRONTEND_URL = 'acciaioli.duckdns.org'
