5acba53f8f09502c555dd80f5d3e7121062c79cd
Joao Valverde <acci.valverde@gmail.com>
2018-12-01 16:09:54 +0000

    updated jwt response payload handler

--- auth/jwt_utils/test.py
+++ auth/jwt_utils/test.py
@@ -3,7 +3,7 @@ from django.test import TestCase
 
 from rest_framework_jwt.settings import api_settings
 
-from jwt_utils.handlers import jwt_payload_handler
+from jwt_utils.handlers import jwt_payload_handler, jwt_response_payload_handler
 
 User = get_user_model()
 
@@ -15,7 +15,7 @@ USER_CHI = {
 }
 
 
-class TestHandler(TestCase):
+class TestPayLoadHandler(TestCase):
 
     def test_settings(self):
         self.assertEqual(api_settings.JWT_PAYLOAD_HANDLER, jwt_payload_handler)
@@ -30,3 +30,26 @@ class TestHandler(TestCase):
         self.assertIn('exp', payload)
         self.assertEqual(payload['username'], USER_CHI['username'])
         self.assertEqual(payload['email'], USER_CHI['email'])
+
+
+class TestResponsePayLoadHandler(TestCase):
+
+    def test_settings(self):
+        self.assertEqual(api_settings.JWT_RESPONSE_PAYLOAD_HANDLER, jwt_response_payload_handler)
+
+    def test_payload_generation(self):
+        user_chi = User.objects.create_user(**USER_CHI)
+
+        payload = jwt_response_payload_handler('dummy_token', user_chi)
+
+        self.assertEqual(len(payload), 2)
+        self.assertIn('token', payload)
+        self.assertIn('user', payload)
+
+        self.assertIn(payload['token'], 'dummy_token')
+
+        self.assertIn('uuid', payload['user'])
+        self.assertIn('username', payload['user'])
+        self.assertIn('email', payload['user'])
+        self.assertEqual(payload['user']['username'], USER_CHI['username'])
+        self.assertEqual(payload['user']['email'], USER_CHI['email'])
